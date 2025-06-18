10ff058c33b5b30d7ca42bc975a74c84338ff159
Usama Ayoub <usamabinayoub@gmail.com>
2025-06-18 15:46:28 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+from langchain_mcp_adapters import load_mcp_tools
+from langgraph import create_react_agent
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+import asyncio
+import os
+
+load_dotenv()
